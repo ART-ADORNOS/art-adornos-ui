102b612417a18f931a397e5019ff6502351bec65
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-02-20 23:36:09 -0500

    refactor(App): :wrench: refactor code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import {useState, useEffect} from "react";
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-import {AuthProvider} from './shared/providers/AuthContext';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider } from './shared/providers/AuthContext';
 import LandingPages from "./modules/landing/pages/landingPages";
 import NotFoundPage from "./shared/components/errors/NotFoundPage";
 import ThemeContext from "./shared/providers/ThemeContent";
@@ -10,13 +10,21 @@ import Register from './modules/auth/pages/register/register';
 import Dashboard from './modules/dashboard/pages/dashboard/Dashboard';
 import DashboardSeller from './modules/dashboard/pages/userSeller/DashboardSeller';
 import UpdateProfile from './modules/dashboard/pages/dashboard/updateProfile';
-import {NotificationProvider} from "./shared/providers/alertProvider";
-import RegisterStartup from "./modules/dashboard/pages/startup/registerStartup";
+import { NotificationProvider } from "./shared/providers/alertProvider";
+import RegisterStartup from "./modules/startup/pages/registerStartup";
+import ProductList from "./modules/products/pages/ProductList";
 
+export const protectedRoutes: { path: string, component: React.ReactNode }[] = [
+    { path: "/dashboard", component: <Dashboard /> },
+    { path: "/edit-profile", component: <UpdateProfile /> },
+    { path: "/dashboard-seller", component: <DashboardSeller /> },
+    { path: "/register-startup", component: <RegisterStartup /> },
+    { path: "/product-list", component: <ProductList /> },
+];
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     const token = localStorage.getItem('token');
-    return token ? children : <Navigate to="/login"/>;
+    return token ? children : <Navigate to="/login" />;
 };
 
 function App() {
@@ -30,7 +38,7 @@ function App() {
     }, []);
 
     function toggleTheme() {
-        setIsDarkMode(prevMode => {
+        setIsDarkMode((prevMode) => {
             const newMode = !prevMode;
             localStorage.setItem("isDarkMode", newMode.toString());
             return newMode;
@@ -38,49 +46,24 @@ function App() {
     }
 
     return (
-        <ThemeContext.Provider value={{isDarkMode, toggleTheme}}>
+        <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
             <div className={`${isDarkMode ? "dark" : "light"} min-h-screen flex flex-col`}>
                 <NotificationProvider>
                     <AuthProvider>
                         <Router>
                             <Routes>
-                                <Route path="/" element={<LandingPages/>}/>
-                                <Route path="/register" element={<Register/>}/>
-                                <Route path="/login" element={<Login/>}/>
-                                <Route
-                                    path="/dashboard"
-                                    element={
-                                        <ProtectedRoute>
-                                            <Dashboard/>
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route
-                                    path="/edit-profile"
-                                    element={
-                                        <ProtectedRoute>
-                                            <UpdateProfile/>
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route
-                                    path="/dashboard-seller"
-                                    element={
-                                        <ProtectedRoute>
-                                            <DashboardSeller/>
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route
-                                    path="/register-startup"
-                                    element={
-                                        <ProtectedRoute>
-                                            <RegisterStartup/>
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route path="/admin" element={<LoginAdmin/>}/>
-                                <Route path="*" element={<NotFoundPage/>}/>
+                                <Route path="/" element={<LandingPages />} />
+                                <Route path="/register" element={<Register />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/admin" element={<LoginAdmin />} />
+                                <Route path="*" element={<NotFoundPage />} />
+                                {protectedRoutes.map((route, index) => (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={<ProtectedRoute>{route.component}</ProtectedRoute>}
+                                    />
+                                ))}
                             </Routes>
                         </Router>
                     </AuthProvider>
