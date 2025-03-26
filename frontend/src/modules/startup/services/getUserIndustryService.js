c334521e6e0b904008b6cfc6f099f4f5f2f3a609
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-03-25 22:25:13 -0500

    feat(Industry): :sparkles: Update industry service URLs and enhance FilterSidebar layout with scrollbar styling

--- frontend/src/modules/startup/services/getUserIndustryService.js
+++ frontend/src/modules/startup/services/getUserIndustryService.js
@@ -1,9 +1,9 @@
 import apiStore from "../../../core/api/ApiStore";
-import {BASE_URLS_STARTUP} from "../../../core/constants/startup/urlsStartup";
+import {BASE_URLS_INDUSTRY} from "../../../core/constants/industry/urlsIndustry";
 
 
 export const getUserIndustry = async () => {
-    const response = await apiStore.get(BASE_URLS_STARTUP.GET_USER_INDUSTRY);
+    const response = await apiStore.get(BASE_URLS_INDUSTRY.GET_USER_INDUSTRY);
     if (response.status === 200) {
         return response.data;
     } else {
