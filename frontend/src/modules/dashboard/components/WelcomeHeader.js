8cead1eb31bd242f62c2357829264e2cb0b8e266
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-04-01 23:00:55 -0500

    WIP(Cart): :construction: shopbutton component

--- frontend/src/modules/dashboard/components/WelcomeHeader.js
+++ frontend/src/modules/dashboard/components/WelcomeHeader.js
@@ -1,11 +1,18 @@
 import AddButton from "../../../shared/components/buttons/AddButton";
+import ShopButton from "./ShopButton";
 
 
 export default function WelcomeHeader({username, redirectTo = "", title = ""}) {
-    return (<div className="flex flex-col sm:flex-row justify-between items-center px-8 py-4">
-        <div className="text-3xl sm:text-4xl font-extrabold text-gray-800 dark:text-white mb-4 sm:mb-0">
-            ¡Bienvenido, <span className="text-blue-600">{username}</span>! 👋
+    return (
+        <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-4">
+            <div className="text-3xl sm:text-4xl font-extrabold text-gray-800 dark:text-white mb-4 sm:mb-0">
+                ¡Bienvenido, <span className="text-blue-600">{username}</span>! 👋
+            </div>
+            {redirectTo && title ? (
+                <AddButton redirectTo={redirectTo} title={title}/>
+            ) : (
+                <ShopButton redirectTo={redirectTo}/>
+            )}
         </div>
-        {redirectTo && title && (<AddButton redirectTo={redirectTo} title={title}/>)}
-    </div>)
+    )
 }
