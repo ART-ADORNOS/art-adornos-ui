91f8d14f035e162c4428e2206ec7aeb8fbfd8ef2
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-04-10 21:54:30 -0500

    feat(ProductInput): :sparkles: Refactor layout and styling of Product input fields for improved user experience

--- frontend/src/modules/products/components/ProductInput.js
+++ frontend/src/modules/products/components/ProductInput.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import {FiUploadCloud} from "react-icons/fi";
 
 const ProductInput = ({formData, handleChange, categories}) => {
-
     const [previewImage, setPreviewImage] = useState(null);
 
     const handleImageChange = (event) => {
@@ -15,80 +14,87 @@ const ProductInput = ({formData, handleChange, categories}) => {
             reader.readAsDataURL(file);
             handleChange(event);
         }
-    }
+    };
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-800">Nombre</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder="Nombre del producto"
-                    className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
-                />
+            <div className="space-y-6">
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-800">Nombre</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        placeholder="Nombre del producto"
+                        className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
+                    />
+                </div>
+
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-800">Categoría</label>
+                    <select
+                        name="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                        className="w-full p-3 border rounded-md bg-white text-gray-900"
+                    >
+                        <option value="">Seleccione una opción</option>
+                        {categories.map((cat, i) => (
+                            <option key={i} value={cat.name}>
+                                {cat.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-800">Precio</label>
+                        <input
+                            type="text"
+                            name="price"
+                            value={formData.price}
+                            onChange={handleChange}
+                            placeholder="Precio"
+                            className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-800">Stock</label>
+                        <input
+                            type="text"
+                            name="stock"
+                            value={formData.stock}
+                            onChange={handleChange}
+                            placeholder="Stock"
+                            className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
+                        />
+                    </div>
+                </div>
+
+
+                <div>
+                    <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">
+                        Descripción
+                    </label>
+                    <textarea
+                        rows="5"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                        placeholder="Enter your content"
+                        id="content"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    ></textarea>
+                </div>
             </div>
 
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-800">Categoría</label>
-                <select
-                    name="category"
-                    value={formData.category}
-                    onChange={handleChange}
-                    className="w-full p-3 border rounded-md bg-white text-gray-900"
-                >
-                    <option value="">Seleccione una opción</option>
-                    {categories.map((cat, i) => (
-                        <option key={i} value={cat.name}>{cat.name}</option>
-                    ))}
-                </select>
-            </div>
-
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-800">Precio</label>
-                <input
-                    type="text"
-                    name="price"
-                    value={formData.price}
-                    onChange={handleChange}
-                    placeholder="Precio"
-                    className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
-                />
-            </div>
-
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-800">Stock</label>
-                <input
-                    type="text"
-                    name="stock"
-                    value={formData.stock}
-                    onChange={handleChange}
-                    placeholder="Stock"
-                    className="w-full p-3 border rounded-md bg-gray-50 text-gray-900"
-                />
-            </div>
-
-            <div className="md:col-span-2">
-                <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">
-                    Descripción
-                </label>
-                <textarea
-                    rows="5"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleChange}
-                    placeholder="Enter your content"
-                    id="content"
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                ></textarea>
-            </div>
-
-            <div className="md:col-span-2">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Imagen</label>
+            <div className="flex flex-col items-center justify-center">
                 <div
-                    className="w-full border-2 border-dashed border-gray-300 rounded-lg p-6 flex flex-col items-center justify-center text-center bg-gray-50 hover:bg-gray-100 transition cursor-pointer">
+                    className="w-full border-2 border-dashed border-gray-300 rounded-lg p-6 flex flex-col items-center justify-center text-center bg-gray-50 hover:bg-gray-100 transition cursor-pointer"
+                >
                     <input
                         type="file"
                         name="image"
@@ -101,11 +107,9 @@ const ProductInput = ({formData, handleChange, categories}) => {
                         <FiUploadCloud className="text-blue-600 w-12 h-12 mb-4"/>
 
                         <span className="text-blue-600 font-semibold">
-                            Sube una imagen o arrastra y suelta <u>aquí</u>
+                          Sube una imagen o arrastra y suelta <u>aquí</u>
                         </span>
-                        <span className="text-gray-400 text-sm mt-1">
-                            Soporta: JPG, JPEG2000, PNG
-                        </span>
+                        <span className="text-gray-400 text-sm mt-1">Soporta: JPG, JPEG2000, PNG</span>
                     </label>
                     {previewImage && (
                         <img
@@ -116,7 +120,6 @@ const ProductInput = ({formData, handleChange, categories}) => {
                     )}
                 </div>
             </div>
-
         </div>
     );
 };
