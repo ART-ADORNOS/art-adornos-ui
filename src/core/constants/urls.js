889385b32d41905d2353b808bc70a1a457d8b771
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-12-26 00:07:31 -0500

    feat(config): :sparkles: add jsconfig.json for improved module resolution and update axios import paths

--- src/core/constants/urls.js
+++ src/core/constants/urls.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL_ACCOUNTS = 'http://127.0.0.1:8000/accounts';
-export const BASE_URL_STORE = 'http://127.0.0.1:8000/store';
+export const BASE_URL_ACCOUNTS = 'http://localhost:8000/accounts';
+export const BASE_URL_STORE = 'http://localhost:8000/store';
--- src/modules/auth/services/registerService.js
+++ src/modules/auth/services/registerService.js
@@ -1,5 +1,5 @@
-import axios from "../../../../core/api/axios";
-import {BASE_URLS_USER} from "../../../../core/constants/user/urlsUser";
+import axios from '../../../core/api/axios'
+import {BASE_URLS_USER} from "../../../core/constants/user/urlsUser";
 
 const registerUser = async (formData) => {
     await axios.post(BASE_URLS_USER.REGISTER_USER, formData);
