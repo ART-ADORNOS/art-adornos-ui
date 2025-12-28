a67cc6670f620106c293606b2a72891949c2c4fb
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-12-28 09:48:16 -0500

    feat(auth): :sparkles: refactor authService for improved URL management and modularity

--- src/modules/auth/services/authService.js
+++ src/modules/auth/services/authService.js
@@ -1,7 +1,8 @@
 import accountsApi from '../../../core/api/accountsApi';
+import {BASE_URLS_AUTH} from "../../../core/constants/auth/urlsAuth";
 
 export const loginRequest = async (username, password) => {
-  const { data } = await accountsApi.post('/api/token/', {
+  const { data } = await accountsApi.post(BASE_URLS_AUTH.LOGIN, {
     username,
     password,
   });
@@ -9,6 +10,6 @@ export const loginRequest = async (username, password) => {
 };
 
 export const getCurrentUser = async () => {
-  const { data } = await accountsApi.get('/api/me/');
+  const { data } = await accountsApi.get(BASE_URLS_AUTH.CURRENT_USER);
   return data;
 };
